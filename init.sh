359dc69e97193005dfd963725ce3dcc5a60d0c17
Wesley Klop <wesley19097@gmail.com>
2020-04-08 22:45:30 +0200

    Add control script, db and env file, cleanup code style

--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git clone git@github.com:WesleyKlop/journali-frontend.git frontend
-git clone git@github.com:WesleyKlop/journali-api.git api
-
---
+++ journali.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo "      _                              _ _ 
+     | |                            | (_)
+     | | ___  _   _ _ __ _ __   __ _| |_ 
+ _   | |/ _ \| | | | '__| '_ \ / _\` | | |
+| |__| | (_) | |_| | |  | | | | (_| | | |
+ \____/ \___/ \__,_|_|  |_| |_|\__,_|_|_|
+"
+
+BASE_COMMAND="docker-compose -f docker-compose.builder.yml run --rm"
+
+case $1 in
+    init)
+        echo " - Initializing repository"
+        git clone git@github.com:WesleyKlop/journali-frontend.git frontend
+        git clone git@github.com:WesleyKlop/journali-api.git api
+        ;;
+    install)
+        echo " - Installing frontend dependencies for the frontend"
+        eval $BASE_COMMAND install
+        ;;
+    migrate)
+        echo " - Migrating database"
+        # TODO
+        ;;
+    start)
+        echo " - Starting development environment"
+        docker-compose up -d
+        ;;
+    stop)
+        echo " - Stopping development environment"
+        docker-compose down
+        ;;
+    *)
+        echo "Unknown command \"$1\""
+        exit 1
+esac
